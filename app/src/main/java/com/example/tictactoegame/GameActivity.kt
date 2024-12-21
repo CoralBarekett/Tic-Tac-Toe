26a403f3c9934da799276ede8f37722a1858508c
CoralBarekett <coral.bareket@cs.colman.ac.il>
2024-12-21 20:54:55 +0200

    Add initial game board setup (work in progress)

--- app/src/main/java/com/example/tictactoegame/GameActivity.kt
+++ app/src/main/java/com/example/tictactoegame/GameActivity.kt
@@ -1,29 +1,20 @@
 package com.example.tictactoegame
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_game)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val startGameButton: Button = findViewById(R.id.main_activity_start_game_button)
-        startGameButton.setOnClickListener {
-            val intent = Intent(this, GameActivity::class.java)
-            startActivity(intent)
         }
     }
 }
